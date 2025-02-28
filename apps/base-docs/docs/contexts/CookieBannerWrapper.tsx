cc29d325d92bd0bfe8471a6f53f91e56a0d287c9
Brendan Forster <brendan.forster@coinbase.com>
2025-02-27 16:35:38 -0800

    revert cookiebanner change

--- apps/base-docs/docs/contexts/CookieBannerWrapper.tsx
+++ apps/base-docs/docs/contexts/CookieBannerWrapper.tsx
@@ -3,7 +3,8 @@ import ClientAnalyticsScript from '@/components/ClientAnalyticsScript/ClientAnal
 import { isDevelopment } from '@/constants.ts';
 
 // CJS import
-import { CookieBanner } from '@coinbase/cookie-banner';
+import pkg from '@coinbase/cookie-banner';
+const { CookieBanner } = pkg;
 
 export const cookieBannerTheme = {
   colors: {
