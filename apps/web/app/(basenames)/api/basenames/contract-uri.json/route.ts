b7df8196d1df6927e27d04fd8497a7ebeb416760
Brendan Forster <brendan.forster@coinbase.com>
2025-04-16 16:59:01 -0700

    contract-uri.json

--- apps/web/app/(basenames)/api/basenames/contract-uri.json/route.ts
+++ apps/web/app/(basenames)/api/basenames/contract-uri.json/route.ts
@@ -1,23 +1,19 @@
-import { isDevelopment } from 'apps/web/src/constants';
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import { base } from 'viem/chains';
+import { getChain } from 'apps/web/src/utils/basenames/getChain';
+import { getDomain } from 'apps/web/src/utils/basenames/getDomain';
 
-export const config = {
-  runtime: 'edge',
-};
+export const runtime = 'edge';
 
-export default async function GET(request: Request) {
-  const url = new URL(request.url);
-  const domainName = isDevelopment ? `${url.protocol}//${url.host}` : 'https://www.base.org';
-
-  const chainIdFromParams = url.searchParams.get('chainId');
-  const chainId = chainIdFromParams ? Number(chainIdFromParams) : base.id;
-  if (!chainId) return NextResponse.json({ error: '406: chainId is missing' }, { status: 406 });
-
-  const name = chainId === base.id ? 'Basename' : 'Basename (Sepolia testnet)';
+export async function GET(request: NextRequest) {
+  const domainName = getDomain(request);
+  const chainId = getChain(request);
+  if (!chainId) {
+    return NextResponse.json({ error: '400: chainId is missing' }, { status: 400 });
+  }
 
   const tokenMetadata = {
-    name: name,
+    name: chainId === base.id ? 'Basename' : 'Basename (Sepolia testnet)',
     description:
       'Basenames are a core onchain building block that enables anyone to establish their identity on Base by registering human-readable names for their address(es). They are a fully onchain solution which leverages ENS infrastructure deployed on Base.',
     image: `${domainName}/images/basenames/contract-uri/logo.png`,
