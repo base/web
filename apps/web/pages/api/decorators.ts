b579ff808cbfadb80d072bfec8c70d66863adb8b
Brendan Forster <brendan.forster@coinbase.com>
2025-04-17 09:50:07 -0700

    update withTimeout for next15

--- apps/web/pages/api/decorators.ts
+++ apps/web/pages/api/decorators.ts
@@ -1,41 +1,41 @@
 import { logger } from 'apps/web/src/utils/logger';
-import { NextApiRequest, NextApiResponse } from 'next';
+import { NextRequest, NextResponse } from 'next/server';
 
-type NextApiHandler = (req: NextApiRequest, res: NextApiResponse) => void | Promise<void>;
+type NextApiHandler = (req: NextRequest) => Promise<NextResponse>;
 
 const defaultTimeout = process.env.DEFAULT_API_TIMEOUT ?? 5000;
 export function withTimeout(
   handler: NextApiHandler,
   timeoutLimit = defaultTimeout,
 ): NextApiHandler {
-  return async (req, res) => {
-    const timeoutPromise = new Promise((_, reject) =>
+  return async (req) => {
+    const timeoutPromise = new Promise<never>((_, reject) =>
       setTimeout(() => reject(new Error('Request timed out')), timeoutLimit as number),
     );
 
-    const handlerPromise = new Promise<void>((resolve, reject) => {
-      Promise.resolve(handler(req, res))
-        .then(() => resolve())
+    const handlerPromise = new Promise<NextResponse>((resolve, reject) => {
+      Promise.resolve(handler(req))
+        .then((response) => resolve(response))
         .catch((error) => reject(error));
     });
 
     try {
-      await Promise.race([handlerPromise, timeoutPromise]);
+      return await Promise.race([handlerPromise, timeoutPromise]);
     } catch (error) {
       if (error instanceof Error) {
         if (error.message === 'Request timed out') {
           logger.error('Request timed out', error, {
             endpoint_url: req.url,
-            params: req.query,
+            params: req.nextUrl.searchParams,
           });
-          return res.status(408).json({ error: 'Request timed out' });
+          return NextResponse.json({ error: 'Request timed out' }, { status: 408 });
         }
       }
       logger.error('Error in withTimeout', error, {
         endpoint_url: req.url,
-        params: req.query,
+        params: req.nextUrl.searchParams,
       });
-      return res.status(500).json({ error: 'Something went wrong' });
+      return NextResponse.json({ error: 'Something went wrong' }, { status: 500 });
     }
   };
 }
