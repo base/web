08988df20cf5a9d7d71ad44da9b2d013877916c0
Brendan Forster <brendan.forster@coinbase.com>
2025-04-18 16:52:13 -0700

    wrap datastores operations in try-catch

--- apps/web/app/api/registry/featured/route.ts
+++ apps/web/app/api/registry/featured/route.ts
@@ -7,33 +7,38 @@ import { withTimeout } from 'apps/web/app/api/decorators';
 const PAGE_KEY = 'api.ocs_registry.featured';
 
 async function handler() {
-  const db = getVercelDb();
-  const content = await db
-    .selectFrom('content')
-    .where('is_featured', '=', true)
-    .selectAll()
-    .limit(1)
-    .execute();
-
-  const row = content[0];
-
-  const response = {
-    data: row,
-  };
-
   try {
+    const db = getVercelDb();
+    const content = await db
+      .selectFrom('content')
+      .where('is_featured', '=', true)
+      .selectAll()
+      .limit(1)
+      .execute();
+
+    const row = content[0];
+
+    const response = {
+      data: row,
+    };
+
+    // Increment request count
     const kv = getKv();
     await kv.incr(`stat:requests.${PAGE_KEY}`);
+
+    // Set caching headers
+    return NextResponse.json(response, {
+      headers: {
+        'Cache-Control': 's-maxage=300, stale-while-revalidate',
+      },
+    });
   } catch (error) {
     logger.error('error getting featured registry entries', error);
+    return NextResponse.json(
+      { error: `Error fetching featured registry entries: ${error}` },
+      { status: 500 },
+    );
   }
-
-  // Set caching headers
-  return NextResponse.json(response, {
-    headers: {
-      'Cache-Control': 's-maxage=300, stale-while-revalidate',
-    },
-  });
 }
 
 export const GET = withTimeout(handler);
