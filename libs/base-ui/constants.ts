88c5877d30fc384af004e18f74016e22bcfa411c
pinkBoss <iotas.echoes-6o@icloud.com>
2025-05-07 21:17:19 +0200

    Update constants.ts

--- libs/base-ui/constants.ts
+++ libs/base-ui/constants.ts
@@ -1,6 +1,6 @@
 export const nodeEnv = process.env.NODE_ENV;
 export const docsUrl = process.env.DOCS_URL ?? 'https://docs.base.org';
-export const bridgeUrl = process.env.BRIDGE_URL ?? 'https://bridge.base.org';
+export const bridgeUrl = process.env.BRIDGE_URL ?? 'https://docs.base.org/chain/bridges-mainnet';
 export const greenhouseApiUrl =
   process.env.GREENHOUSE_HTTPS ?? 'https://boards-api.greenhouse.io/v1';
 export const mainnetLaunchBlogPostURL =
