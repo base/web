46cff2b9726ce9539bed81dbe0bde38c9ecfc498
katzman <steve.katzman@coinbase.com>
2025-05-12 11:48:58 -0700

    Rename fetch method to match new api

--- apps/web/src/components/Basenames/UsernameProfileSectionBadges/hooks/useTalentProtocol.ts
+++ apps/web/src/components/Basenames/UsernameProfileSectionBadges/hooks/useTalentProtocol.ts
@@ -1,6 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
 
-async function fetchPassport(address: `0x${string}`): Promise<Data> {
+async function fetchScore(address: `0x${string}`): Promise<Data> {
   const response = await fetch(`/api/basenames/talentprotocol/${address}`);
   const data = (await response.json()) as Data;
   return data;
@@ -17,7 +17,7 @@ type Data = {
 export function useTalentProtocol(address?: `0x${string}`) {
   const query = useQuery<Data>({
     queryKey: ['talent-protocol', address],
-    queryFn: async ({ queryKey }) => fetchPassport(queryKey[1] as `0x${string}`),
+    queryFn: async ({ queryKey }) => fetchScore(queryKey[1] as `0x${string}`),
     enabled: !!address,
   });
 
