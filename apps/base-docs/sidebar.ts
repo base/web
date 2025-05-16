cbd27927fc45cf59ad558e8c76b3a6fce0d358d6
youssefea <youcefea99@gmail.com>
2025-05-16 01:56:29 +0100

    fix sub accounts reference

--- apps/base-docs/sidebar.ts
+++ apps/base-docs/sidebar.ts
@@ -957,7 +957,7 @@ export const sidebar: Sidebar = [
               },
               {
                 text: 'Sub Accounts Reference',
-                link: '/identity/smart-wallet/technical-reference/sdk/sub-account-reference',
+                link: '/identity/smart-wallet/technical-reference/sub-account-reference',
               },
             ],
           },
