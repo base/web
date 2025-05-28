08d4712f389455780cb5be53823ed6230225db0a
Arjun Dureja <arjun.dureja@coinbase.com>
2025-05-28 12:37:10 -0700

    update

--- apps/web/src/components/Basenames/RenewalFlow.tsx
+++ apps/web/src/components/Basenames/RenewalFlow.tsx
@@ -1,5 +1,4 @@
 'use client';
-import dynamic from 'next/dynamic';
 import { Transition } from '@headlessui/react';
 import RegistrationBackground from 'apps/web/src/components/Basenames/RegistrationBackground';
 import RenewalForm from 'apps/web/src/components/Basenames/RenewalForm';
@@ -9,13 +8,10 @@ import useBasenameChain, { supportedChainIds } from 'apps/web/src/hooks/useBasen
 import classNames from 'classnames';
 import { useCallback, useEffect, useMemo } from 'react';
 import { useAccount, useSwitchChain } from 'wagmi';
-import { isDevelopment } from 'libs/base-ui/constants';
 import { Basename } from '@coinbase/onchainkit/identity';
-
-const RegistrationStateSwitcherDynamic = dynamic(
-  async () => import('apps/web/src/components/Basenames/RegistrationStateSwitcher'),
-  { ssr: false },
-);
+import { Icon } from 'apps/web/src/components/Icon/Icon';
+import { useRouter } from 'next/navigation';
+import { RegistrationSteps } from 'apps/web/src/components/Basenames/RegistrationContext';
 
 type RenewalFlowProps = {
   name: string;
@@ -25,6 +21,7 @@ export function RenewalFlow({ name }: RenewalFlowProps) {
   const { chain } = useAccount();
   const { basenameChain } = useBasenameChain();
   const { switchChain } = useSwitchChain();
+  const router = useRouter();
 
   const isOnSupportedNetwork = useMemo(
     () => chain && supportedChainIds.includes(chain.id),
@@ -35,6 +32,10 @@ export function RenewalFlow({ name }: RenewalFlowProps) {
     () => switchChain({ chainId: basenameChain.id }),
     [basenameChain.id, switchChain],
   );
+
+  const onBackArrowClick = useCallback(() => {
+    router.back();
+  }, [router]);
 
   useEffect(() => {
     if (!chain || !switchToIntendedNetwork) {
@@ -60,60 +61,68 @@ export function RenewalFlow({ name }: RenewalFlowProps) {
   const currentUsernamePillVariant = UsernamePillVariants.Inline;
 
   return (
-    <>
-      {true && isDevelopment && <RegistrationStateSwitcherDynamic />}
-      <section className={mainClasses}>
-        {/* Username Pill */}
-        <div className="relative flex w-full max-w-full flex-col items-center justify-center md:-translate-y-12">
-          <Transition
-            appear
-            show
-            className={classNames(
-              'relative z-10 w-full max-w-full transition-opacity',
-              renewalTransitionDuration,
-            )}
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            {/* The pill */}
-            <Transition
-              appear
-              show
-              className={classNames(
-                'transition-[max-width, transform] mx-auto',
-                renewalTransitionDuration,
-              )}
-              enterFrom="max-w-0"
-              enterTo="max-w-full"
+    <section className={mainClasses}>
+      {/* Username Pill */}
+      <div className="relative flex w-full max-w-full flex-col items-center justify-center md:-translate-y-12">
+        <Transition
+          appear
+          show
+          className={classNames(
+            'relative z-10 w-full max-w-full transition-opacity',
+            renewalTransitionDuration,
+          )}
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="absolute left-1/2 z-9 mx-auto w-full -translate-x-1/2 -translate-y-[calc(15vh)] gap-4 px-2 transition-opacity md:max-w-[16rem] md:-translate-y-20">
+            <button
+              onClick={onBackArrowClick}
+              type="button"
+              aria-label="Find another name"
+              className="flex items-center gap-6"
             >
-              <UsernamePill variant={currentUsernamePillVariant} username={name as Basename} />
-            </Transition>
-          </Transition>
-
-          {/* Renewal Form */}
+              <Icon name="backArrow" color="currentColor" height="1rem" width="1rem" />
+              <p className="text-gray-50">EXTEND REGISTRATION</p>
+            </button>
+          </div>
+          {/* The pill */}
           <Transition
             appear
             show
             className={classNames(
-              'relative z-40 transition-opacity',
-              'mx-auto',
+              'transition-[max-width, transform] mx-auto',
               renewalTransitionDuration,
             )}
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
+            enterFrom="max-w-0"
+            enterTo="max-w-full"
           >
-            <RenewalForm name={name} />
+            <UsernamePill variant={currentUsernamePillVariant} username={name as Basename} />
           </Transition>
-        </div>
+        </Transition>
 
-        {/* Animated background for each step */}
-        <RegistrationBackground />
-      </section>
-    </>
+        {/* Renewal Form */}
+        <Transition
+          appear
+          show
+          className={classNames(
+            'relative z-40 transition-opacity',
+            'mx-auto',
+            renewalTransitionDuration,
+          )}
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <RenewalForm name={name} />
+        </Transition>
+      </div>
+
+      {/* The renewal flow should only display the Claim background */}
+      <RegistrationBackground registrationStep={RegistrationSteps.Claim} />
+    </section>
   );
 }
 
