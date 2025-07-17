393fa50c4b154e447fea24e53e062190283c94c7
Aushveen Vimalathas <aushveen.vimalathas@coinbase.com>
2025-07-17 15:26:19 -0700

    enable parallelism for E2E tests, 4 tests = 4 workers

--- apps/web/playwright.config.ts
+++ apps/web/playwright.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 6 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: 1,
+  workers: 4,
   maxFailures: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
