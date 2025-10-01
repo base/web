a73c0bcb323f40c356633f6b52b88ba8a232a68d
Auster Chen <teaonbase@gmail.com>
2025-10-01 16:10:13 -0700

    fix

--- apps/web/src/components/Layout/Navigation/GasPriceDropdown.tsx
+++ apps/web/src/components/Layout/Navigation/GasPriceDropdown.tsx
@@ -7,7 +7,7 @@ import { useGasPrice } from 'wagmi';
 import dynamic from 'next/dynamic';
 
 const SimpleCryptoProvider = dynamic(
-  () => import('apps/web/app/SimpleCryptoProviders').then((mod) => mod.SimpleCryptoProvider),
+  () => import('apps/web/app/SimpleCryptoProviders').then(async (mod) => mod.SimpleCryptoProvider),
   {
     ssr: false,
   },
