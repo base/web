ca65a00af407a6c3f5a6a12a8644b59cf8038f98
Arjun Dureja <arjun.dureja@coinbase.com>
2026-01-09 20:17:19 -0500

    Add layout.test.tsx

--- apps/web/jest.config.js
+++ apps/web/jest.config.js
@@ -11,6 +11,7 @@ const customJestConfig = {
   moduleNameMapper: {
     '^@/components/(.*)$': '<rootDir>/components/$1',
     '^@/pages/(.*)$': '<rootDir>/pages/$1',
+    '^apps/web/(.*)$': '<rootDir>/$1',
   },
   testPathIgnorePatterns: ['<rootDir>/e2e/'],
 };
