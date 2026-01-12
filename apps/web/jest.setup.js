2aeaf45f67f1c9effb046f60698d42c27cf51c76
Arjun Dureja <arjun.dureja@coinbase.com>
2026-01-11 19:48:28 -0500

    update

--- apps/web/jest.setup.js
+++ apps/web/jest.setup.js
@@ -1,8 +1,5 @@
 require('@testing-library/jest-dom');
 
-// Configure React to use React.act instead of deprecated ReactDOMTestUtils.act
-globalThis.IS_REACT_ACT_ENVIRONMENT = true;
-
 // Polyfill TextEncoder/TextDecoder for viem compatibility
 const { TextEncoder, TextDecoder } = require('util');
 global.TextEncoder = TextEncoder;
